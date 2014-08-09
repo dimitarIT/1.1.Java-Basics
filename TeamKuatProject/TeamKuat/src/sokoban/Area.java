723c4fec4743a00d405398bc7548aff0e5b792fb
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2014-08-09 09:32:07 +0300

    Team Project

---
+++ TeamKuatProject/TeamKuat/src/sokoban/Area.java
@@ -0,0 +1,18 @@
+package sokoban;
+
+import java.awt.Image;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+
+public class Area extends Actor {
+
+	public Area(int x, int y) {
+		super(x, y);
+
+		URL loc = this.getClass().getResource("dot.png");
+		ImageIcon iia = new ImageIcon(loc);
+		Image image = iia.getImage();
+		this.setImage(image);
+	}
+}
